d106026648611b75556af242d39a3e9262f229dd
Noah <noahchun77@gmail.com>
2023-08-02 16:44:52 -0400

    added read button

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 let myLibrary = [];
 
-function Book(name) { // Book constructor
+function Book(name, author, pages, read) { // Book constructor
     this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read || false;
 }
+
+Book.prototype.toggleReadStatus = function() {
+    this.read = !this.read;
+};
+
+Book.prototype.generateCardHTML = function() {
+    const readStatus = this.read ? 'Yes' : 'No';
+    return `
+        <h3>${this.name}</h3>
+        <p>Author: ${this.author}</p>
+        <p>Pages: ${this.pages}</p>
+        <p data-read-status>Read: ${readStatus}</p>
+        <button class="read-button">Read</button>
+        <button class="remove-button">Remove</button>
+    `;
+};
 
 const addBookButton = document.querySelector('.addBookButton');
 
@@ -58,7 +77,46 @@ function closeModal(modal) {
     overlay.classList.remove('active');
 }
 
-const submitButton = documnent.querySelector('.submit-button');
+const submitButton = document.querySelector('.submit-button');
+const bookContainer = document.querySelector('.book-container');
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const didRead = document.getElementById('checkbox').checked;
 
+    const newBook = new Book(title, author, pages, didRead);
+    myLibrary.push(newBook);
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = newBook.generateCardHTML();
+
+    bookContainer.appendChild(card);
+
+    const modal = document.querySelector('.modal');
+    closeModal(modal);
+});
+
+bookContainer.addEventListener('click', function(event) {
+    if (event.target.classList.contains('remove-button')) {
+        const card = event.target.closest('.card');
+        if (card) {
+            bookContainer.removeChild(card);
+        }
+    }
+    if (event.target.classList.contains('read-button')) {
+        const card = event.target.closest('.card');
+        if (card) {
+            const bookIndex = Array.from(bookContainer.children).indexOf(card);
+            if (bookIndex !== -1) {
+                myLibrary[bookIndex].toggleReadStatus(); 
+                const readStatus = card.querySelector('p[data-read-status]');
+                readStatus.textContent = `Read: ${myLibrary[bookIndex].read ? 'Yes' : 'No'}`;
+            }
+        }
+    }
+});
 
 console.log(myLibrary);
